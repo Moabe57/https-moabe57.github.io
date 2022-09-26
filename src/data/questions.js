6e01b5a476257b9eb9270a18f8065aea75e7ba4c
Moabe Souza <moabe57@gmail.com>
2022-09-26 20:35:49 -0300

    Test

--- src/data/questions.js
+++ src/data/questions.js
@@ -6,10 +6,10 @@ let questions = [
       type: "multiple",
       difficulty: "easy",
       question: "What does HTML stand for?",
-      answer: "Hyper Text Markup Language",
+      answer: "teste",
       options: [
         "Hyper Text Preprocessor",
-        "Hyper Text Markup Language",
+        "teste",
         "Hyper Text Multiple Language",
         "Hyper Tool Multi Language",
       ],
