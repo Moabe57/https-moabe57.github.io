3b75da6c252af18c189a0034943331c9bb724620
Moabe Souza <moabe57@gmail.com>
2022-09-28 22:38:51 -0300

    Quase acabando

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -222,11 +222,11 @@ function startTimer(time) {
 }
 
 function startTimerLine(time) {
-  counterLine = setInterval(timer, 29);
+  counterLine = setInterval(timer, 47);
   function timer() {
     time += 1; //upgrading time value with 1
     time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-    if (time > 549) {
+    if (time > 340) {
       //if time value is greater than 549
       clearInterval(counterLine); //clear counterLine
     }
